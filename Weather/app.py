cec2b6f5ea2dae4333637955252cbf248f560935
soumilshah1995 <soushah@my.bridgeport.edu>
2019-07-05 21:28:39 -0400

    added search bar

--- Weather/app.py
+++ Weather/app.py
@@ -1,16 +1,32 @@
 from flask import render_template, url_for, request
+from wtforms import StringField, SubmitField
 from flask import Flask
+from flask_wtf import FlaskForm
 from weatherdata import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
 
 
 @app.route('/', methods=["POST", "GET"])
 def index():
     w = Weather()
+    form = Form()
     data = w.weather_get(zip='06604')
-    return render_template("home.html", data=data)
 
+    if request.method == "POST":
+
+        if form.validate_on_submit():
+
+            txt_search = form.search.data
+            data = w.weather_get(zip=txt_search)
+            return render_template("home.html", data=data, form=form)
+    else:
+        return render_template("home.html", data=data, form=form)
+
+class Form(FlaskForm):
+    button = SubmitField('Search')
+    search = StringField('submit')
 
 if __name__ == "__main__":
     app.run(debug=True)
